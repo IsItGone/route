d460a11e0f493ebcad21b9d584849c85da63c4d0
Sanghun <ph38230119@gmail.com>
2024-08-03 17:08:33 +0900

    [Fix] Test profile 수정
    test 시 test profile 설정 파일을 사용하도록 수정

--- src/test/java/com/ddd/route/RouteApplicationTests.java
+++ src/test/java/com/ddd/route/RouteApplicationTests.java
@@ -2,12 +2,14 @@ package com.ddd.route;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RouteApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
